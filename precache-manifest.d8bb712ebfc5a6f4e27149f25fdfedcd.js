73cb5b771e1f5020f59d8aa22c3569eaa721048b
chinmayi sunku <chinmayi.sunku@sjsu.edu>
2023-09-08 09:30:29 -0700

    Updates

---
+++ precache-manifest.d8bb712ebfc5a6f4e27149f25fdfedcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa51026d7c555d3536c7b3de6eb5a46",
+    "url": "/ChinmayiPortfolio/index.html"
+  },
+  {
+    "revision": "70e64f8adf73cc81aa89",
+    "url": "/ChinmayiPortfolio/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "f512bbe6da2c9f8ce2b1",
+    "url": "/ChinmayiPortfolio/static/css/main.ff29f905.chunk.css"
+  },
+  {
+    "revision": "70e64f8adf73cc81aa89",
+    "url": "/ChinmayiPortfolio/static/js/2.1c5472e9.chunk.js"
+  },
+  {
+    "revision": "b8a26cc07166dc30cf9055bb104c91cd",
+    "url": "/ChinmayiPortfolio/static/js/2.1c5472e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f512bbe6da2c9f8ce2b1",
+    "url": "/ChinmayiPortfolio/static/js/main.83ffd7f9.chunk.js"
+  },
+  {
+    "revision": "aadef156d878b53329fb",
+    "url": "/ChinmayiPortfolio/static/js/runtime-main.af034481.js"
+  }
+]);
