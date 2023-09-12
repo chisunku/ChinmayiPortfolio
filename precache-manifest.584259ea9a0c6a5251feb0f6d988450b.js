46c1facc02ced34b7c025a560a172ada9352f31b
chinmayi sunku <chinmayi.sunku@sjsu.edu>
2023-09-12 11:16:53 -0700

    Updates

--- precache-manifest.584259ea9a0c6a5251feb0f6d988450b.js
+++ precache-manifest.584259ea9a0c6a5251feb0f6d988450b.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "afa51026d7c555d3536c7b3de6eb5a46",
+    "revision": "d622f2c315934f629a0276b5ef3e775b",
     "url": "/ChinmayiPortfolio/index.html"
   },
   {
-    "revision": "70e64f8adf73cc81aa89",
+    "revision": "b8dea5372a72139e4df5",
     "url": "/ChinmayiPortfolio/static/css/2.3eb2a443.chunk.css"
   },
   {
-    "revision": "f512bbe6da2c9f8ce2b1",
+    "revision": "637bf9bf5c3536d901d6",
     "url": "/ChinmayiPortfolio/static/css/main.ff29f905.chunk.css"
   },
   {
-    "revision": "70e64f8adf73cc81aa89",
-    "url": "/ChinmayiPortfolio/static/js/2.1c5472e9.chunk.js"
+    "revision": "b8dea5372a72139e4df5",
+    "url": "/ChinmayiPortfolio/static/js/2.a351322d.chunk.js"
   },
   {
     "revision": "b8a26cc07166dc30cf9055bb104c91cd",
-    "url": "/ChinmayiPortfolio/static/js/2.1c5472e9.chunk.js.LICENSE.txt"
+    "url": "/ChinmayiPortfolio/static/js/2.a351322d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f512bbe6da2c9f8ce2b1",
-    "url": "/ChinmayiPortfolio/static/js/main.83ffd7f9.chunk.js"
+    "revision": "637bf9bf5c3536d901d6",
+    "url": "/ChinmayiPortfolio/static/js/main.f8dc911e.chunk.js"
   },
   {
     "revision": "aadef156d878b53329fb",
